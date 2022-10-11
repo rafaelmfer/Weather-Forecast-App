3935570506e3d51298a7e724f94f74f699784c55
Rafael Ferreira <rafaelmfer@hotmail.com>
2022-10-10 22:05:28 -0300

    :sparkles: feat: creation of home screen

--- app/src/main/java/com/rafaelmfer/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/rafaelmfer/weatherforecast/MainActivity.kt
@@ -2,11 +2,14 @@ package com.rafaelmfer.weatherforecast
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.rafaelmfer.weatherforecast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
